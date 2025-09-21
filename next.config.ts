d781a71a4589339e33c6ee936cff5d4ce60298fb
Logan Br <SemNomeLogan@gmail.com>
2025-09-21 18:55:06 +0000

    ✓ Starting...  ⚠ Invalid next.config.ts options detected:  ⚠     Unrecog

--- next.config.ts
+++ next.config.ts
@@ -24,10 +24,11 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
-  experimental: {
-    // This can help in environments with slow filesystems (e.g. some antivirus configurations, network drives)
-    watchPolling: true,
-  },
+  // This can help in environments with slow filesystems (e.g. some antivirus configurations, network drives)
+  watchOptions: {
+    poll: 1000,
+    aggregateTimeout: 300,
+  }
 };
 
 export default nextConfig;
